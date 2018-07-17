591c9d17c6ff366a6e2754273e4d90c2db94d7a9
Vincent Perlerin <vperlerin@gmail.com>
2018-07-17 15:37:58 +0200

    Delete smtp.php

--- application/conf/vm/setup/smtp.php
+++
@@ -1,8 +0,0 @@
-<?php
- 
-// SMTP
-define('SMTP_HOST','smtp.gmail.com');
-define('SMTP_USER','amsmeteors@gmail.com');
-define('SMTP_PWD','m3t30r1911');
-define('SMTPSecure','tls');
-define('SMTP_TLS_PORT','587');
